932ab9f763a1ce50cbc0b4b551b960b5c9578334
Borkanie <43271944+Borkanie@users.noreply.github.com>
2022-09-24 20:57:14 +0300

    Adds online solution to numberOfZeros.

--- NumberOfZerosInFactorial/main.cpp
+++ NumberOfZerosInFactorial/main.cpp
@@ -22,17 +22,31 @@ int numberOfZeros(int x)
     return zeros;
 }
 
+int onlineSolution(int factorialnumber)
+{
+    int zero_count = 0;
+    for (int five_factor = 5;
+         five_factor <= factorialnumber;
+         five_factor *= 5)
+    {
+        zero_count += factorialnumber / five_factor;
+    }
+    return zero_count;
+}
+
 int numberOfZerosFast(int x)
 {
     int zeros = 0;
     // we go trough all the multiples of 5 smaller than our number and count the fives
     // we don't need to count the 2's because there are always more twos than fives in a factiorial
-    for (int i = 1; i <= x/5; i++)
+    for (int i = 1; i <= x / 5; i++)
     {
-        if(i%5==0){
-            zeros += i/5+1;//plus one comes from the fact we already dived by 5;
+        if (i % 5 == 0)
+        {
+            zeros += i / 5 + 1; // plus one comes from the fact we already dived by 5;
         }
-        else{
+        else
+        {
             zeros += 1;
         }
     }
@@ -46,7 +60,10 @@ int main()
     char wait;
     int target;
     std::cin >> target;
-    std::cout << "Number of zeros is: " << numberOfZerosFast(target);
+    int result1 = numberOfZerosFast(target);
+    std::cout << "Number of zeros is: " << result1 << std::endl;
+    int result2 = onlineSolution(target);
+    std::cout << "Number of zeros is with fast: " << result1 << std::endl;
     std::cin >> wait;
     return 0;
 }
